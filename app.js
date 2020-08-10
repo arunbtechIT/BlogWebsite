059a8cd86906de3abc037b887418e3f601ac72d1
arun@21 <44541856+arun2128@users.noreply.github.com>
2020-08-11 02:49:32 +0530

    firstcommit3

--- app.js
+++ app.js
@@ -68,6 +68,6 @@ app.get("/posts/:postId", function(req, res){
 
 });
 
-app.listen(3000, function() {
+app.listen(3000||process.env.PORT, function() {
   console.log("Server started on port 3000");
 });
